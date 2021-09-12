7fa585e7291ad104f3f5b066b91062d9ce0d512b
Thiago Lima <thiagolima.unb@gmail.com>
2021-09-12 13:15:16 -0300

    add bank-account

---
+++ elixir/bank-account/lib/bank_account.ex
@@ -0,0 +1,71 @@
+defmodule BankAccount do
+  use GenServer
+
+  @moduledoc """
+  A bank account that supports access from multiple processes.
+  """
+
+  @typedoc """
+  An account handle.
+  """
+  @opaque account :: pid
+
+  @doc """
+  Open the bank. Makes the account available.
+  """
+  @spec open_bank() :: account
+  def open_bank() do
+    {:ok, pid} = GenServer.start(__MODULE__, 0)
+    pid
+  end
+
+  @doc """
+  Close the bank. Makes the account unavailable.
+  """
+  @spec close_bank(account) :: none
+  def close_bank(account) do
+    GenServer.stop(account)
+  end
+
+  @doc """
+  Get the account's balance.
+  """
+  @spec balance(account) :: integer
+  def balance(account) do
+    case Process.alive?(account) do
+      true -> GenServer.call(account, :balance)
+      false -> {:error, :account_closed}
+    end
+  end
+
+  @doc """
+  Update the account's balance by adding the given amount which may be negative.
+  """
+  @spec update(account, integer) :: any
+  def update(account, amount) do
+    case Process.alive?(account) do
+      true -> GenServer.cast(account, {:update, amount})
+      false -> {:error, :account_closed}
+    end
+  end
+
+  @impl GenServer
+  def init(state) do
+    {:ok, state}
+  end
+
+  @impl GenServer
+  def handle_call(:balance, _, account) do
+    {:reply, account, account}
+  end
+
+  @impl GenServer
+  def handle_cast({:update, amount}, account) do
+    {:noreply, account + amount}
+  end
+
+  @impl GenServer
+  def terminate(:normal, account) do
+    {:account_closed, account}
+  end
+end
